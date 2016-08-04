0131c86e7b1cf16938c7737e7f0ad9524059c586
Tarun Krishnan <tarun@colossal.net>
2016-08-04 11:01:23 -0400

    initial commit

---
+++ readScroll-v1.0.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var creditArray = {
+		name: "Tarun Krishnan",
+		url: "http://tarunkrishnan.com",
+		script_name: 'ReadScroll',
+		created: "August 4, 2016"
+	};
+	console.log()
+});
