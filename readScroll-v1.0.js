c269ebf738b6f1271488f0be67d5ca9f6324db9e
Tarun Krishnan <tarun@colossal.net>
2016-08-04 21:25:11 -0400

    detecting when body copy reached the top

--- readScroll-v1.0.js
+++ readScroll-v1.0.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var creditArray = {
 		name: "Tarun Krishnan",
 		url: "http://tarunkrishnan.com",
-		script_name: 'ReadScroll',
+		script_name: 'tk1z_birdhousebranding_carve_readscroll',
 		created: "August 4, 2016"
 	};
 	var blkstr = [];
@@ -13,12 +13,24 @@ $(document).ready(function(){
 	console.log(blkstr.join(", "));
 
 	var contentBlocks = $('.entry-content').find('.sqs-block-html');
+	var $window = $(window);
+	var distance;
+	var mainContentBlock;
 	var counter = 0;
+	var contentHeight;
 	contentBlocks.each(function(){
 	  counter++;
 	  if(counter == 2) {
-	    console.log($(this).height());
+	  	contentHeight = $(this).height();
+	  	mainContentBlock = $(this);
+	  	distance = mainContentBlock.offset().top;
 	  }
 	});
+
+	$window.scroll(function() {
+    if ( $window.scrollTop() >= distance ) {
+        console.log('start counting!');
+    }
+});
 
 });
